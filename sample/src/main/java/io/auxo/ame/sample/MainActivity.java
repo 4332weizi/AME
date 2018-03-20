30a645302dd7d270ebb3bf3c6b86f4c99cf444a3
Victor Chiu <4332weizi@gmail.com>
2018-03-20 13:00:50 +0800

    增加权限处理

--- sample/src/main/java/io/auxo/ame/sample/MainActivity.java
+++ sample/src/main/java/io/auxo/ame/sample/MainActivity.java
@@ -1,15 +1,18 @@
 package io.auxo.ame.sample;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import io.auxo.ame.lite.Mp3Encoder;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected final int REQUEST_PERMISSIONS = 1;
 
     private Button mAmeLite;
     private Button mAme;
@@ -24,6 +27,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mAmeLite.setOnClickListener(this);
         mAme.setOnClickListener(this);
+
+        requestPermissions();
+    }
+
+    @AfterPermissionGranted(REQUEST_PERMISSIONS)
+    protected void requestPermissions() {
+        String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            mAme.setEnabled(true);
+            mAmeLite.setEnabled(true);
+        } else {
+            EasyPermissions.requestPermissions(this, "Mp3 Encoder需要读写权限",
+                    REQUEST_PERMISSIONS, perms);
+            mAme.setEnabled(false);
+            mAmeLite.setEnabled(false);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
     @Override
